6e2775b8fdac8784b064d5fd5097073a9e1966c2
majitomoomin <mariajoseportill2@gmail.com>
2024-04-27 12:54:43 -0600

    cimmit #4

--- WinFormsApp2_ejercicio/WinFormsApp2_ejercicio/Form1.cs
+++ WinFormsApp2_ejercicio/WinFormsApp2_ejercicio/Form1.cs
@@ -9,7 +9,12 @@ namespace WinFormsApp2_ejercicio
 
         private void buttonCalcular_Click(object sender, EventArgs e)
         {
+            int Monto_Inicial = int.Parse(textBoxMontoIn.Text);
+            int Tasa_Interes = int.Parse(textBoxIntereses.Text);
+            int Tiempo = int.Parse(textBoxTiempo.Text);
+            int Capital_Final = ;
 
+            labelTotal.Text = $"El capital final del ahorro a plazo fijo es: {Capital_Final}";
         }
     }
 }
